5d1254260f624846b9fafd0ce0ba4905597e9a84
limxuan <reconlx.official@gmail.com>
2025-04-24 16:30:31 +0800

    fake quiz explore
    - quickly navigate to quiz pages

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,11 +1,19 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
-export default function QuizPage() {
-  const router = useRouter();
+export default async function QuizPage() {
+  const supabase = await createClient();
+  const { data } = await supabase.from("quizzes").select("quiz_id");
+  console.log(data);
   return (
-    <>
-      <div className="flex-1 w-full flex flex-col gap-12">Quiz Page</div>
-    </>
+    <div className="space-y-2">
+      {data?.map((quiz) => (
+        <Link key={quiz.quiz_id} href={`/quiz/${quiz.quiz_id}`}>
+          <div className="text-blue-600 hover:underline">
+            Quiz {quiz.quiz_id}
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 }
