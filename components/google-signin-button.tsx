7b868da8ca8ddb1fb370305a03209fb9163153c9
limxuan <reconlx.official@gmail.com>
2025-05-02 18:51:25 +0800

    fix: vercel prod redirect 2

--- components/google-signin-button.tsx
+++ components/google-signin-button.tsx
@@ -4,13 +4,25 @@ import { createClient } from "@/utils/supabase/client";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url + "/complete-profile";
+};
+
 export default function GoogleSignInButton() {
   const handleGoogleSignIn = async () => {
     const supabase = createClient();
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/complete-profile`,
+        redirectTo: getURL(),
       },
     });
   };
